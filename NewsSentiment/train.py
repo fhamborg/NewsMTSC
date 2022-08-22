fhamborg/NewsMTSC

commit 213c1da0aa61f0c390767a3bea8b1f0827bbd4ca

add confidence to evaluator

NewsSentiment/train.py
@@ -174,6 +174,7 @@ class Instructor:
             SentimentClasses.get_sorted_expected_label_values(),
             SentimentClasses.get_polarity_associations(),
             self.opt.snem,
+            self.opt.is_return_confidence,
         )
 
         self._print_args()
@@ -752,16 +753,18 @@ class Instructor:
             y_pred = self._get_classes_from_sequence_output(
                 t_outputs_all, t_text_bert_indices_targets_mask_all
             ).cpu()
+            t_outputs_confidence = None
         else:
             # softmax: get predictions from outputs
             # have to take the 3rd (dim=2) dimension
             if self.opt.is_return_confidence:
                 # we have to remove the last class because this is the confidence and
                 # we do not want to evaluate on the confidence (at least not now)
+                t_outputs_confidence = t_outputs_all.clone().cpu()[:, :, -1]
                 t_outputs_all = t_outputs_all[:, :, :-1]
             y_pred = torch.argmax(t_outputs_all, dim=2).cpu()
 
-        stats = self.evaluator.calc_statistics(y_true, y_pred)
+        stats = self.evaluator.calc_statistics(y_true, y_pred, t_outputs_confidence)
 
         if get_examples:
             self.evaluator.write_error_table(

.idea/inspectionProfiles/Project_Default.xml
@@ -1,15 +1,0 @@
-<component name="InspectionProjectProfileManager">
-  <profile version="1.0">
-    <option name="myName" value="Project Default" />
-    <inspection_tool class="PyPep8Inspection" enabled="true" level="WEAK WARNING" enabled_by_default="true">
-      <option name="ignoredErrors">
-        <list>
-          <option value="W29" />
-          <option value="E501" />
-          <option value="W29" />
-          <option value="E501" />
-        </list>
-      </option>
-    </inspection_tool>
-  </profile>
-</component>

NewsSentiment/evaluator.py
@@ -8,10 +8,17 @@ from sklearn import metrics
 from NewsSentiment.SentimentClasses import SentimentClasses
 from NewsSentiment.dataset import FXDataset
 from NewsSentiment.fxlogger import get_logger
+import torch
 
 
 class Evaluator:
-    def __init__(self, sorted_expected_label_values, polarity_associations, snem_name):
+    def __init__(
+        self,
+        sorted_expected_label_values,
+        polarity_associations,
+        snem_name,
+        is_return_confidence,
+    ):
         self.logger = get_logger()
         self.polarity_associations = polarity_associations
         self.pos_label_value = polarity_associations["positive"]
@@ -24,6 +31,7 @@ class Evaluator:
             self.neg_label_value
         )
         self.snem_name = snem_name
+        self.is_return_confidence = is_return_confidence
 
     def mean_from_all_statistics(self, all_test_stats):
         # for Counters, we do not take the mean
@@ -48,7 +56,7 @@ class Evaluator:
 
         return mean_test_stats
 
-    def calc_statistics(self, y_true, y_pred):
+    def calc_statistics(self, y_true, y_pred, y_pred_confidence):
         """
         Calculates performance statistics by comparing k-dimensional Tensors y_true and
         y_pred. Both y_true and y_pred's shape have to be(batchsize, maxtargetsperexample)
@@ -67,17 +75,66 @@ class Evaluator:
         # target as an example
         y_true = y_true.view(-1)
         y_pred = y_pred.view(-1)
+        if y_pred_confidence is not None:
+            y_pred_confidence = y_pred_confidence.view(-1)
 
         # in both tensors, keep only those scalars that are non-fill in y_true
         non_fillup_mask = y_true != SentimentClasses.FILLUP_POLARITY_VALUE
         y_true = y_true[non_fillup_mask]
         y_pred = y_pred[non_fillup_mask]
+        if y_pred_confidence is not None:
+            y_pred_confidence = y_pred_confidence[non_fillup_mask].tolist()
 
         # this is just the previous, single-target evaluation logic
         y_true_list = y_true.tolist()
         y_pred_list = y_pred.tolist()
         y_true_count = Counter(y_true_list)
         y_pred_count = Counter(y_pred_list)
+        confidence_info = {}
+
+        # perform confidence evaluation if vector was given
+        if y_pred_confidence is not None:
+            # compare where equal for confidence evaluation
+            is_correct_list = []
+            for index in range(len(y_true_list)):
+                y_true_item = y_true_list[index]
+                y_pred_item = y_pred_list[index]
+                if y_true_item == y_pred_item:
+                    is_correct_list.append(1)
+                else:
+                    is_correct_list.append(0)
+            # now we have a list whether the prediction is correct (1) or not (0)
+            # let's compare it with the confidence
+
+            # regression metrics
+            mse = metrics.mean_squared_error(is_correct_list, y_pred_confidence)
+            r2_score = metrics.r2_score(is_correct_list, y_pred_confidence)
+
+            # convert to classification problem (uses 0.5 threshold)
+            y_pred_confidence_classification = (
+                torch.FloatTensor(y_pred_confidence) > 0.5
+            ).tolist()
+
+            f1_macro_conf = metrics.f1_score(
+                is_correct_list, y_pred_confidence_classification, average="macro"
+            )
+            accuracy_conf = metrics.accuracy_score(
+                is_correct_list, y_pred_confidence_classification
+            )
+            f1_of_classes = metrics.f1_score(
+                is_correct_list, y_pred_confidence_classification, average=None
+            ).tolist()
+
+            confidence_info = {
+                "f1m": f1_macro_conf,
+                "acc": accuracy_conf,
+                "f1_classes": f1_of_classes,
+                "mse": mse,
+                "r2score": r2_score,
+                "y_pred_confidence_classification": y_pred_confidence_classification,
+                "y_pred_confidence": y_pred_confidence,
+                "y_true_confidence": is_correct_list,
+            }
 
         f1_macro = metrics.f1_score(
             y_true, y_pred, labels=self.sorted_expected_label_values, average="macro"
@@ -103,7 +160,7 @@ class Evaluator:
         )
         accuracy = metrics.accuracy_score(y_true, y_pred)
 
-        return {
+        results = {
             "f1_macro": f1_macro,
             "confusion_matrix": confusion_matrix,
             "recalls_of_classes": recalls_of_classes,
@@ -115,6 +172,10 @@ class Evaluator:
             "y_true_count": y_true_count,
             "y_pred_count": y_pred_count,
         }
+        if y_pred_confidence is not None:
+            results["confidence_info"] = confidence_info
+
+        return results
 
     def print_stats(self, stats, description):
         self.logger.info(description)

commit a8e077afabc9ad1b9bd9ae2af4698b38d4d4b4f3

Fix UnboundLocalError thrown during training.
NewsSentiment/train.py --own_model_name grutsc --dataset_name
newsmtsc-rw threw this error as local referenced before assignment in
some situations.

Looks like an oversight when this variable was introduced in 213c1da0

NewsSentiment/train.py
@@ -692,6 +692,7 @@ class Instructor:
         t_outputs_all = None
         t_text_bert_indices_targets_mask_all = None
         t_texts_all = []
+        t_outputs_confidence = None
 
         # switch model to evaluation mode
         self.own_model.eval()